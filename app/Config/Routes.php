563ea99830bcf1d3018864abb2b7f5bfbfe1a362
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2025-06-03 19:49:59 +0300

    ci4 users

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,8 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/lietotaji', 'Home::listUsers');
+$routes->get('/users', 'Home::listUsers');
+
+// endpoints
+
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,11 +1,82 @@
 <?php
 
 namespace App\Controllers;
-
 class Home extends BaseController
+
 {
-    public function index(): string
+    public function index()
+        
     {
-        return view('welcome_message');
+        $data = [
+            'success' => true,
+            'products' => [
+                [
+                    'name'=>"Burkani",
+                    'id'      => 123,
+                ],
+                [
+                    'name'=>"Katupeļi",
+                    'id'      => 124,
+                ],
+                [
+                    'name'=>"Kafija",
+                    'id'      => 124,
+                ],
+            ]
+        ];
+
+
+        return $this->response->setJSON($data);
+
+        // return view('welcome_message');
     }
+
+
+    public function listUsers($id = ""){
+             $users = [
+            [
+                'id'    => 1,
+                'name'  => 'Anna Ozoliņa',
+                'email' => 'anna@example.com',
+            ],
+            [
+                'id'    => 2,
+                'name'  => 'Jānis Bērziņš',
+                'email' => 'janis@example.com',
+            ],
+            [
+                'id'    => 3,
+                'name'  => 'Laura Kalniņa',
+                'email' => 'laura@example.com',
+            ],
+            [
+                'id'    => 4,
+                'name'  => 'Mārtiņš Liepa',
+                'email' => 'martins@example.com',
+            ],
+            [
+                'id'    => 5,
+                'name'  => 'Ilze Lapiņa',
+                'email' => 'ilze@example.com',
+            ],
+        ];
+
+        
+        if($id==""){
+                 return $this->response->setJSON($users);
+        }else{
+            $users_filtered = array_filter($users,function($item){
+                if($item["id"]==$id){
+                    return $item;
+                }
+            });
+            return $this->response->setJSON($users_filtered);
+        }
+
+         
+
+        
+    }
+
+
 }
